01062c93eec6a14fdf394d20a644e42cd8164629
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-08-18 23:42:10 +0530

    CREATE second-random.py

---
+++ second-random.py
@@ -0,0 +1,12 @@
+class MyClass:
+    def __init__(self):
+        self.message = "Hello, world!"
+
+    def print_message(self):
+        print(self.message)
+
+# Create an instance of the class
+obj = MyClass()
+
+# Call the print_message method
+obj.print_message()
